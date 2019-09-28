6f82e32e3deda29d407972d1415da7455f8b6aff
Markslaw <Shaunalily9@gmail.com>
2019-09-28 17:51:58 +0100

    refactored email configuration class as well as got email Notification working as an observer

--- Loan/src/main/java/Observer/Observer.java
+++ Loan/src/main/java/Observer/Observer.java
@@ -5,6 +5,6 @@ import java.sql.Timestamp;
 
 public interface Observer {
 	
-    public void update(String customerName,double amount,Date transactionDate,Timestamp transactionTime);
+    public void update(String customerName,double amount,Date transactionDate,Timestamp transactionTime, String transactionCountry);
     
 }
