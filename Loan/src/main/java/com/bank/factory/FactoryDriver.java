b08a9f188cca3e41c7b2d5e107bb8b78f0a67f51
Markslaw <Shaunalily9@gmail.com>
2019-12-30 21:51:18 +0000

    Added Factory method plus Enums for type safety

--- Loan/src/main/java/com/bank/factory/FactoryDriver.java
+++ Loan/src/main/java/com/bank/factory/FactoryDriver.java
@@ -1,5 +1,23 @@
 package com.bank.factory;
 
-public class FactoryDriver {
+import com.bank.decorator.LoanFixedCharges;
+import com.bank.decorator.LoanInsuranceStandardPolicy;
+import com.bank.decorator.LoanTaxLocalCustomer;
+import com.bank.decorator.MidTermCustomerDiscount;
+import com.bank.loan.Mortgage;
 
-}
+public class FactoryDriver {
+	
+	public static void main (String args[]) {
+
+	LoanFactory additionalCharges = new LoanFactory();
+	LoanFixedCharges bla = additionalCharges.getLoanFixedCharges(LoanFixedChargesType.MORT_TAXLC_LONGTCD);
+	System.out.println(bla.additionalExpenses());
+	System.out.println(bla.getLoanFixedChargesDescription());
+	
+	
+        	
+	}
+        }
+
+
--- Loan/src/main/java/com/bank/factory/LoanFactory.java
+++ Loan/src/main/java/com/bank/factory/LoanFactory.java
@@ -1,5 +1,43 @@
 package com.bank.factory;
 
-public class LoanFactory {
+import com.bank.decorator.LoanFixedCharges;
+import com.bank.decorator.LoanInsuranceStandardPolicy;
+import com.bank.decorator.LoanTaxLocalCustomer;
+import com.bank.decorator.MidTermCustomerDiscount;
+import com.bank.loan.Mortgage;
 
-}
+	public class LoanFactory {
+		
+		LoanFixedCharges mortgage = new Mortgage();
+		
+		
+		public LoanFixedCharges getLoanFixedCharges(LoanFixedChargesType loanFixedChargesType) {
+			
+
+	        switch (loanFixedChargesType) {
+	            case MORT_TAXLC_LONGTCD:
+	            	mortgage = new LoanTaxLocalCustomer(mortgage);
+		    		mortgage = new LoanInsuranceStandardPolicy(mortgage);
+		    		mortgage = new MidTermCustomerDiscount(mortgage);
+		    		return mortgage;
+
+	            default:
+	                return null;
+			
+		}
+	        
+		}
+	        
+	        
+	      public LoanFixedCharges Calc() {
+	        	mortgage = new LoanTaxLocalCustomer(mortgage);
+	    		mortgage = new LoanInsuranceStandardPolicy(mortgage);
+	    		mortgage = new MidTermCustomerDiscount(mortgage);
+	    		return mortgage;
+	        }
+	        	
+	        }
+
+
+	
+
