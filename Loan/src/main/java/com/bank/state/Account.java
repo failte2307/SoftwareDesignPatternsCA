dcbacc1c321c805c03200d4679b07b0954db270f
Markslaw <Shaunalily9@gmail.com>
2020-01-01 18:40:37 +0000

    had HighInterestAccount take on method of state

--- Loan/src/main/java/com/bank/state/Account.java
+++ Loan/src/main/java/com/bank/state/Account.java
@@ -2,4 +2,14 @@ package com.bank.state;
 
 public class Account {
 
+    private int accountId;
+	private String accountHolder;
+	private double balance;
+	
+	
+	 AccountState highInterestAccountState;
+	 AccountState lowInterestAccountState;
+	 AccountState OverDrawnAccountState;
+	 AccountState BlockedAccountState;
+	
 }
