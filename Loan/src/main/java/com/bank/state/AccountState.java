f38eb2b46e26cae70012175fd3466633fab0df99
Markslaw <Shaunalily9@gmail.com>
2020-01-01 15:23:17 +0000

    Created Account State interface to encapsulate methods that the various account states will use

---
+++ Loan/src/main/java/com/bank/state/AccountState.java
@@ -0,0 +1,13 @@
+package com.bank.state;
+
+public interface AccountState {
+	
+	public double Withdraw(double Amount);
+	
+	public double Deposit(double Amount);
+	
+	public double viewBalance();
+	
+	public void checkState();
+
+}
