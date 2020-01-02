99d74000d14d97e67c71f82477e0d9a19754b3ab
Markslaw <Shaunalily9@gmail.com>
2020-01-02 04:25:40 +0000

    Added account controller which will demonstrate singleton and State pattern. Returns an account JSON object that will reflect the interest rate and current state

--- Loan/src/main/java/com/bank/state/AccountController.java
+++ Loan/src/main/java/com/bank/state/AccountController.java
@@ -1,11 +1,13 @@
 package com.bank.state;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.decorator.AdditionalCharges;
+import com.bank.singleton.Logging;
 
 @RestController
 public class AccountController {
@@ -14,5 +16,33 @@ public class AccountController {
 	@ResponseBody
 	public Account getAccountStates(@RequestBody Account account) {
 		try {
-			
+			 Logging.getInstance().infoLog("Current account state: " +account.checkState());
+			 account.getInterestRate();
+			 account.addInterest();	
+			 Logging.getInstance().infoLog("Account Balance is: " + account.getBalance());
+			 
+			 account.withdraw(10499);
+			 Logging.getInstance().infoLog("Current account state: " + account.checkState());
+			 account.addInterest();	
+			 Logging.getInstance().infoLog("Account Balance is: " + account.getBalance());
+			 account.withdraw(10);
+			 
+			 Logging.getInstance().infoLog("Current account state: " + account.checkState());	 
+			 account.deposit(10);
+			 
+			 Logging.getInstance().infoLog("Current account state: " + account.checkState()); 
+			 account.deposit(10000);
+			 
+			 Logging.getInstance().infoLog("Current account state: " + account.checkState());
+			 account.setAccountDetails();
+			 
+			 return account;
 		}
+		catch (Exception e) {
+			Logging.getInstance().errorLog(e.getMessage());
+		}
+			return null;
+	}
+	
+	
+}
